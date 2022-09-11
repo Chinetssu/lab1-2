6e5829210bb429811131849384dfc096d5a2f7f5
sanad <sanad@CHINETSU>
2022-09-11 22:42:19 +0800

    Добавьте файлы проекта.

---
+++ lab1-2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab1_2
+{
+    public class Logic
+    {
+        public static string Search(int amount, int[] sequence)
+        {
+            string result;
+            int adds = -1;
+            int prev = sequence[0];
+            for (int i = 1; i < amount; i++)
+            {
+                if (sequence[i] == prev)
+                {
+                    adds = i;
+                }
+                prev = sequence[i];
+            }
+            if (adds == -1)
+            {
+                result = "Пары одинаковых соседних чисел нет";
+            }
+            else
+            {
+                result = "Пара одинаковых соседних чисел найдена \r\n Их порядковые номера: " + adds + " и " + (adds + 1);
+            }
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дана последовательность натуральных чисел. Определить, есть ли в последовательности хотя бы одна
+            // n-ка одинаковых “соседних” чисел (n и элементы последовательности вводятся с клавиатуры).
+            //В случае положительного ответа определить порядковые номера чисел первой из таких пар.
+            int amount;
+            Console.WriteLine("Введите колличество чисел");
+            amount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите последовательность чисел через Enter:");
+            int[] sequence = new int[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                sequence[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write(Logic.Search(amount,sequence));
+        }
+    }
+}
---
+++ lab1-2Tests/LogicTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab1_2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1_2.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void SearchTestPos()
+        {
+            var amount = 5;
+            var sequence = new int[] { 4, 2, 333, 333, 4 };
+            var result = Logic.Search(amount, sequence);
+            Assert.AreEqual("Пара одинаковых соседних чисел найдена \r\n Их порядковые номера: " + 3 + " и " + 4, result);
+        }
+
+        [TestMethod()]
+        public void SearchTestNeg()
+        {
+            var amount = 5;
+            var sequence = new int[] { 4, 2, 33, 333, 4 };
+            var result = Logic.Search(amount, sequence);
+            Assert.AreEqual("Пары одинаковых соседних чисел нет", result);
+        }
+    }
+}
